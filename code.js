133655f90e80eb57c013f901deaa7ea2c3ee1ba2
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-10-31 14:31:32 -0500

    added prompt

--- code.js
+++ code.js
@@ -1,7 +1,8 @@
 let list = document.getElementsByClassName("video-grid")[0].getElementsByTagName("a");
 
 let a = [];
-for (let i = 0; i < list.length; i++) {
+const e = prompt("which video (number) do you want to start with? (default is 1)","1")
+for (let i = (e-1)*4; i < list.length; i++) {
     if (Math.floor(i / 4) == i / 4) a.push(list[i]);
 }
 
