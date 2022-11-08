374a60465e9cec1112d3b87502c5635b79d68819
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-11-08 08:37:51 -0600

    add channel download

--- code.js
+++ code.js
@@ -1,11 +1,20 @@
 let list = document.getElementsByClassName("video-grid")[0].getElementsByTagName("a");
 
-let a = [];
-const e = prompt("which video (number) do you want to start with? (default is 1)","1")
-for (let i = (e-1)*4; i < list.length; i++) {
-    if (Math.floor(i / 4) == i / 4) a.push(list[i]);
-}
+if (location.href.includes("playlist")) {
+    let a = [];
+    const e = prompt("which video (number) do you want to start with? (default is 1)","1")
+    for (let i = (e-1)*4; i < list.length; i++) {
+        if (Math.floor(i / 4) == i / 4) a.push(list[i]);
+    }
 
-a.forEach(j => {
-    window.open("https://projectlounge.pw/ytdl/download?url=" + j, "_blank");
-})
+    a.forEach(j => {
+        window.open("https://projectlounge.pw/ytdl/download?url=" + j, "_blank");
+    })
+
+} else if (location.href.includes("channel)) {
+    if (window.confirm("download entire channel?")) {
+        Array.from(list).forEach(j => {
+            if (j.href.includes("/watch?v=") && !j.href.includes("listen")) window.open("https://projectlounge.pw/ytdl/download?url=" + j);
+        })
+    }
+}
